e53400a2582cd10ed6cf0204d46c41c1875c4c43
tutanota1 <106948820+tutanota1@users.noreply.github.com>
2022-06-06 13:03:34 +0530

    Update calculator.py

--- plugins/calculator.py
+++ plugins/calculator.py
@@ -3,38 +3,26 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 
-CALCULATE_TEXT = "This is the calculator🎛\n\nPowered by @song_requestgroup ❤️"
+CALCULATE_TEXT = "☎️\n\nBut, no sim card to call🤪\n\nPowered by @song_requestgroup ❤️"
 CALCULATE_BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("DEL", callback_data="DEL"),
-            InlineKeyboardButton("AC", callback_data="AC"),
-            InlineKeyboardButton("(", callback_data="("),
-            InlineKeyboardButton(")", callback_data=")")
-        ],
-        [
             InlineKeyboardButton("7", callback_data="7"),
             InlineKeyboardButton("8", callback_data="8"),
-            InlineKeyboardButton("9", callback_data="9"),
-            InlineKeyboardButton("÷", callback_data="/")
+            InlineKeyboardButton("9", callback_data="9")
         ],
         [
             InlineKeyboardButton("4", callback_data="4"),
             InlineKeyboardButton("5", callback_data="5"),
-            InlineKeyboardButton("6", callback_data="6"),
-            InlineKeyboardButton("×", callback_data="*")
+            InlineKeyboardButton("6", callback_data="6")
         ],
         [
             InlineKeyboardButton("1", callback_data="1"),
             InlineKeyboardButton("2", callback_data="2"),
-            InlineKeyboardButton("3", callback_data="3"),
-            InlineKeyboardButton("-", callback_data="-"),
-        ],
+            InlineKeyboardButton("3", callback_data="3")
         [
-            InlineKeyboardButton(".", callback_data="."),
             InlineKeyboardButton("0", callback_data="0"),
-            InlineKeyboardButton("=", callback_data="="),
-            InlineKeyboardButton("+", callback_data="+"),
+            InlineKeyboardButton("📞", callback_data="+")
         ]
     ]
 )
